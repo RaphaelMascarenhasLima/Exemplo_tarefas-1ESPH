19f75715b56ec178c994e23b86948ae1ce4ad0d8
Raphael Mascarenhas <raphaelmasc@gmail.com>
2026-08-25 11:46:34 -0300

    Update Tarefa.jsx

--- Tarefa.jsx
+++ Tarefa.jsx
@@ -1,10 +1,24 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+
 const Tarefa = () => {
-  return (
-    <>
-      
-    </>
-  )
+
+    //hook useState => manipula o estado da variável
+    const [tarefas, setTarefas] = useState(() => {
+        const salvarTarefas = localStorage.getItem("item-tarefa");
+        return salvarTarefas ? JSON.parse(salvarTarefas) : [];
+    });
+
+    //HOOK useEffecr=> realiza o efeito colateral, nese exemplo vai mostrar a tarefa adicionada em tempo real
+    const [campo, setCampo] = useState("");
+
+    useEffect(() => {
+        localStorage.setItem("item-tarefa", JSON.stringify(tarefas))
+    }, [tarefas])
+    return (
+        <>
+
+        </>
+    )
 }
 
 export default Tarefa
